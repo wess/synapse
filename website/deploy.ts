588652c486d02cd23ee56b561feec7d0b61e0601
Wess Cope <apple@wess.io>
2026-07-29 14:17:11 -0400

    Point the site and README at beta.5

--- website/deploy.ts
+++ website/deploy.ts
@@ -1,6 +1,6 @@
 export const sitepath = "/synapse/";
 export const siteurl = `https://wess.io${sitepath}`;
 export const repositoryurl = "https://github.com/wess/synapse";
-export const releasetag = "v0.1.0-beta.4";
+export const releasetag = "v0.1.0-beta.5";
 export const releaseurl = `${repositoryurl}/releases/tag/${releasetag}`;
 export const downloadurl = `${repositoryurl}/releases/download/${releasetag}/synapse.zip`;
